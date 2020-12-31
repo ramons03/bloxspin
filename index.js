44f2afa368384ccb23795ca4c04a6b982c890baf
ramons03 <ramons_03@hotmail.com>
2020-12-31 12:21:28 -0300

    custom api search

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+const ChannelElement = require('./utils/ChannelElement');
 const YouTube = require('youtube-live-chat');
 const QuickYtSearch = require('quick-yt-search');
 const Discord = require("discord.js");
@@ -9,16 +11,12 @@ const YoutubeSearcher = new QuickYtSearch({
     YtApiKey: config.YOUTUBE_KEY, // Place your YouTube API key here
 });
 
-if(YoutubeSearcher.isVideoUrl('https://www.youtube.com/watch?v=nA6LhIQFPKY') === true) {
-    console.log('OMG, it\'s a video');
-} else {
-    console.log('NOPE');
-};
-YoutubeSearcher.getChannel('spins roblox ').then(channel => {
-    console.log(channel.id);
-    console.log(channel.title);
-    console.log(channel.description);
-});
+// if(YoutubeSearcher.isVideoUrl('https://www.youtube.com/watch?v=nA6LhIQFPKY') === true) {
+//     console.log('OMG, it\'s a video');
+// } else {
+//     console.log('NOPE');
+// };
+
 
 // const yt = new YouTube('UCYXIviXPAaaaU_AOotpXTAw', config.YOUTUBE_KEY);
 
@@ -34,7 +32,20 @@ YoutubeSearcher.getChannel('spins roblox ').then(channel => {
 // yt.on('error', error => {
 //   console.error(error)
 // })
-
+async function buscarCanal(youtubeChannel) {
+    if (!youtubeChannel) {
+        throw new Error('You must enter a search term to find channel.');
+    };
+    try {
+        var urlsearch = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${youtubeChannel}&type=channel&key=${config.YOUTUBE_KEY}&eventType=live`;
+        console.log(urlsearch);
+        const response = await axios.get(urlsearch);
+        //console.log(response);
+        return new ChannelElement(response);
+    } catch (error) {
+        throw new Error('An error occurred while retrieving the channel.');
+    };
+}
 
 
 client.on("message", function(message) {
@@ -53,9 +64,21 @@ client.on("message", function(message) {
         message.reply(`The sum of all the arguments you provided is ${sum}!`);
     }
     else if (command === "buscar") {
-        const numArgs = args.map(x => parseFloat(x));
-        const sum = numArgs.reduce((counter, x) => counter += x);
-        message.reply(`The sum of all the arguments you provided is ${sum}!`);
+        console.log(args);
+        var querystring = args.join(' ');
+        console.log(querystring);
+        try {
+            buscarCanal(args.join(' ')).then(channel => {
+                console.log(channel);
+                console.log(channel.id);
+                console.log(channel.title);
+                console.log(channel.description);
+                message.reply(`canal encontrado ${channel.title}`);
+            });
+        } catch (error) {
+            message.reply(`Error ${error}`);
+        };
+
     }
 });
 
